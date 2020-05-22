523d347fcc45af6dcdf5371a38f86ced93e45aef
ifls <ifls.heyifeng@gmail.com>
2020-05-22 12:23:30 +0800

    删除部分testdata

--- src/bufio/bufio.go
+++ src/bufio/bufio.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package bufio implements buffered I/O. It wraps an io.Reader or io.Writer
+// Package bufio implements buffered I/O. 缓存IO
+// It wraps an io.Reader or io.Writer
 // object, creating another object (Reader or Writer) that also implements
 // the interface but provides buffering and some help for textual I/O.
 package bufio
